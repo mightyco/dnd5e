c2937090a88a430621318ac0af671e4f0777ef98
mightyco <chuck.mcintyre@gmail.com>
2025-04-04 21:51:23 +0000

    Refactor 1v1 combat and tests

--- lib/dnd5e/core/combat.rb
+++ lib/dnd5e/core/combat.rb
@@ -1,38 +1,12 @@
 module Dnd5e
   require_relative "dice"
   module Core
-    class Team
-      attr_reader :name, :members
-
-      def initialize(name:, members: [])
-        @name = name
-        @members = members
-      end
-
-      def add_member(member)
-        @members << member
-        member.team = self
-      end
-
-      def all_members_defeated?
-        @members.all? { |member| !member.statblock.is_alive? }
-      end
-
-      def any_members_alive?
-        @members.any? { |member| member.statblock.is_alive? }
-      end
-
-      def alive_members
-        @members.select { |member| member.statblock.is_alive? }
-      end
-    end
-
-    # Enhanced Combat class
     class Combat
-      attr_reader :teams, :turn_order
+      attr_reader :combatant1, :combatant2, :turn_order
 
-      def initialize(teams:)
-        @teams = teams
+      def initialize(combatant1:, combatant2:)
+        @combatant1 = combatant1
+        @combatant2 = combatant2
         @turn_order = []
       end
 
@@ -40,58 +14,31 @@ module Dnd5e
         roll_initiative
         sort_by_initiative
         until is_over?
-          @turn_order.each do |combatant|
-            take_turn(combatant) if combatant.statblock.is_alive?
-          end
-          end_round
+          take_turn(current_combatant) if current_combatant.statblock.is_alive?
+          switch_turns
         end
         puts "The winner is #{winner.name}"
       end
 
       def roll_initiative
-        @teams.each do |team|
-          team.members.each do |combatant|
-            initiative_roll = Dice.new(1, 20, modifier: combatant.statblock.ability_modifier(:dexterity)).roll.first
-            combatant.instance_variable_set(:@initiative, initiative_roll)
-            @turn_order << combatant
-          end
-        end
+        @combatant1.instance_variable_set(:@initiative, Dice.new(1, 20, modifier: @combatant1.statblock.ability_modifier(:dexterity)).roll.first)
+        @combatant2.instance_variable_set(:@initiative, Dice.new(1, 20, modifier: @combatant2.statblock.ability_modifier(:dexterity)).roll.first)
+        @turn_order = [@combatant1, @combatant2]
       end
 
       def sort_by_initiative
-        @turn_order = @turn_order.sort_by { |combatant| -combatant.instance_variable_get(:@initiative) }
+        @turn_order.sort_by! { |combatant| -combatant.instance_variable_get(:@initiative) }
       end
 
       def take_turn(attacker)
-        # Get all members of other teams that are alive
-        potential_defenders = @teams.reject { |team| team == attacker.team }.flat_map(&:alive_members)
-
-        # If there are no potential defenders, exit
-        return if potential_defenders.empty?
-
-        # Always select the first defender
-        defender = potential_defenders.first
-
-        # Attack the defender
+        defender = attacker == @combatant1 ? @combatant2 : @combatant1
         attack(attacker, defender)
       end
 
       def attack(attacker, defender)
-        # Use the first attack in the attacker's attack list
         attack = attacker.attacks.first
         return if attack.nil?
 
-        attack_roll = attack.calculate_attack_roll(attacker.statblock)
-        if is_hit?(attack_roll, defender)
-          damage = attack.calculate_damage(attacker.statblock)
-          apply_damage(defender, damage)
-          puts "#{attacker.name} hits #{defender.name} for #{damage} damage!"
-        else
-          puts "#{attacker.name} misses #{defender.name}!"
-        end
-      end
-
-      def attack(attacker, defender)
         attack_roll = calculate_attack_roll(attacker)
         if is_hit?(attack_roll, defender)
           damage = calculate_damage(attacker)
@@ -103,7 +50,6 @@ module Dnd5e
       end
 
       def calculate_attack_roll(attacker)
-        # Use the first attack in the attacker's attack list
         attack = attacker.attacks.first
         return if attack.nil?
 
@@ -112,7 +58,6 @@ module Dnd5e
       end
 
       def calculate_damage(attacker)
-        # Use the first attack in the attacker's attack list
         attack = attacker.attacks.first
         return if attack.nil?
 
@@ -129,17 +74,21 @@ module Dnd5e
       end
 
       def is_over?
-        # Check if only one team has members alive
-        @teams.count { |team| team.any_members_alive? } <= 1
+        !@combatant1.statblock.is_alive? || !@combatant2.statblock.is_alive?
       end
 
       def winner
-        # Return the winning team
-        @teams.find { |team| team.any_members_alive? }
+        return @combatant1 if @combatant2.statblock.hit_points <= 0
+        return @combatant2 if @combatant1.statblock.hit_points <= 0
+        nil
       end
 
-      def end_round
-        puts "End of round"
+      def current_combatant
+        @turn_order.first
+      end
+
+      def switch_turns
+        @turn_order.rotate!
       end
     end
   end
--- test/dnd5e/core/test_combat.rb
+++ test/dnd5e/core/test_combat.rb
@@ -25,26 +25,31 @@ module Dnd5e
       def setup
         @hero_statblock = Statblock.new(name: "Hero Statblock", strength: 16, dexterity: 14, constitution: 15, hit_die: "d10", level: 3)
         @goblin_statblock = Statblock.new(name: "Goblin Statblock", strength: 8, dexterity: 14, constitution: 10, hit_die: "d6", level: 1)
-        @sword_attack = Attack.new(name: "Sword", damage_dice: Dice.new(1, 8), relevant_stat: :strength)
-        @bite_attack = Attack.new(name: "Bite", damage_dice: Dice.new(1, 6), relevant_stat: :strength)
+        @sword_attack = TestAttackHit.new(name: "Sword", damage_dice: Dice.new(1, 8), relevant_stat: :strength)
+        @bite_attack = TestAttackMiss.new(name: "Bite", damage_dice: Dice.new(1, 6), relevant_stat: :strength)
 
         @hero = Character.new(name: "Hero", statblock: @hero_statblock, attacks: [@sword_attack])
-        @goblin1 = Monster.new(name: "Goblin 1", statblock: @goblin_statblock, attacks: [@bite_attack])
-        @goblin2 = Monster.new(name: "Goblin 2", statblock: @goblin_statblock, attacks: [@bite_attack])
+        @goblin = Monster.new(name: "Goblin 1", statblock: @goblin_statblock, attacks: [@bite_attack])
 
-        @heroes = Team.new(name: "Heroes", members: [@hero])
-        @goblins = Team.new(name: "Goblins", members: [@goblin1, @goblin2])
-        @combat = Combat.new(teams: [@heroes, @goblins])
+        @combat = Combat.new(combatant1: @hero, combatant2: @goblin)
       end
 
       def test_combat_initialization
-        assert_equal [@heroes, @goblins], @combat.teams
+        assert_equal @combat.combatant1, @hero
+        assert_equal @combat.combatant2, @goblin
         assert_empty @combat.turn_order
+      end
+
+      def test_combat_ends
+        @combat.start
+        assert @combat.is_over?
+        # The hero only hits the goblin only misses
+        assert_equal @hero.name, @combat.winner.name
       end
 
       def test_roll_initiative
         @combat.roll_initiative
-        assert_equal 3, @combat.turn_order.size
+        assert_equal 2, @combat.turn_order.size
         @combat.turn_order.each do |combatant|
           assert combatant.instance_variable_get(:@initiative).is_a?(Integer)
         end
@@ -56,47 +61,39 @@ module Dnd5e
         assert_equal @combat.turn_order.sort_by { |combatant| -combatant.instance_variable_get(:@initiative) }, @combat.turn_order
       end
 
-      def test_take_turn_selects_valid_defender
+      def test_attack_hit_and_miss
         @combat.roll_initiative
         @combat.sort_by_initiative
-        @combat.take_turn(@hero)
-        # This test is hard to assert, but we can check that it doesn't error
-        assert true
-      end
-
-      def test_attack_hits
-        @sword_attack = TestAttackHit.new(name: "Sword", damage_dice: Dice.new(1, 8), relevant_stat: :strength)
-        @bite_attack = TestAttackMiss.new(name: "Bite", damage_dice: Dice.new(1, 6), relevant_stat: :strength)
-
-        @hero = Character.new(name: "Hero", statblock: @hero_statblock, attacks: [@sword_attack])
-        @goblin1 = Monster.new(name: "Goblin 1", statblock: @goblin_statblock, attacks: [@bite_attack])
-        @goblin2 = Monster.new(name: "Goblin 2", statblock: @goblin_statblock, attacks: [@bite_attack])
-
-        @heroes = Team.new(name: "Heroes", members: [@hero])
-        @goblins = Team.new(name: "Goblins", members: [@goblin1, @goblin2])
-        @combat = Combat.new(teams: [@heroes, @goblins])
-
-        @combat.roll_initiative
-        @combat.sort_by_initiative
-        initial_hp = @goblin1.statblock.hit_points
-        @combat.attack(@hero, @goblin1)
-        assert_operator @goblin1.statblock.hit_points, :<, initial_hp
+        initial_hp = @goblin.statblock.hit_points
+        @combat.attack(@hero, @goblin)
+        assert_operator @goblin.statblock.hit_points, :<, initial_hp
         initial_hp = @hero.statblock.hit_points
-        @combat.attack(@goblin2, @hero)
+        @combat.attack(@goblin, @hero)
         assert_equal @hero.statblock.hit_points, initial_hp
       end
 
       def test_is_over
         refute @combat.is_over?
-        @goblin1.statblock.take_damage(@goblin1.statblock.hit_points)
-        @goblin2.statblock.take_damage(@goblin2.statblock.hit_points)
+        @goblin.statblock.take_damage(1000)
         assert @combat.is_over?
       end
 
       def test_winner
-        @goblin1.statblock.take_damage(@goblin1.statblock.hit_points)
-        @goblin2.statblock.take_damage(@goblin2.statblock.hit_points)
-        assert_equal @heroes, @combat.winner
+        @goblin.statblock.take_damage(@goblin.statblock.hit_points)
+        assert_equal @hero.name, @combat.winner.name
+      end
+
+      def test_take_turn_selects_valid_defender
+        20.times do
+            @combat.roll_initiative
+            @combat.sort_by_initiative
+            @combat.turn_order.each do |attacker|
+                initial_hp = @hero.statblock.hit_points
+                defender = @combat.take_turn(attacker)
+                refute_equal attacker, defender, "Attacker should not be the same as the defender"
+                assert_equal @hero.statblock.hit_points, initial_hp
+            end
+        end
       end
     end
   end
