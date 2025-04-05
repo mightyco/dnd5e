9d02e9d2251b9a9bf705921f6236701d438ad346
mightyco <chuck.mcintyre@gmail.com>
2025-04-05 01:12:17 +0000

    Refactor checking

--- lib/dnd5e/core/combat.rb
+++ lib/dnd5e/core/combat.rb
@@ -14,11 +14,10 @@ module Dnd5e
         roll_initiative
         sort_by_initiative
         until is_over?
-          take_turn(current_combatant) if current_combatant.statblock.is_alive?
-          switch_turns if not is_over?
+          take_turn(current_combatant) unless is_over?
+          switch_turns unless is_over?
         end
         puts "The winner is #{winner.name}"
-        return
       end
 
       def roll_initiative
@@ -34,44 +33,32 @@ module Dnd5e
       end
 
       def take_turn(attacker)
-        return if not attacker.statblock.is_alive?
-
         defender = attacker == @combatant1 ? @combatant2 : @combatant1
         attack(attacker, defender)
       end
 
       def attack(attacker, defender)
-        return if not (attacker.statblock.is_alive? || defender.statblock.is_alive?)
+        # return unless attacker.statblock.is_alive? && defender.statblock.is_alive?
         attack = attacker.attacks.first
         return if attack.nil?
 
-        attack_roll = calculate_attack_roll(attacker)
+        attack_roll = calculate_attack_roll(attacker, attack)
         if is_hit?(attack_roll, defender)
-          damage = calculate_damage(attacker)
+          damage = calculate_damage(attacker, attack)
           apply_damage(defender, damage)
           puts "#{attacker.name} hits #{defender.name} for #{damage} damage!"
-          if not defender.statblock.is_alive?
-            puts "#{defender.name} is defeated!"
-          end
+          puts "#{defender.name} is defeated!" unless defender.statblock.is_alive?
         else
           puts "#{attacker.name} misses #{defender.name}!"
         end
       end
 
-      def calculate_attack_roll(attacker)
-        attack = attacker.attacks.first
-        return if attack.nil?
-
-        attack_roll = attack.calculate_attack_roll(attacker.statblock)
-        attack_roll
+      def calculate_attack_roll(attacker, attack)
+        attack.calculate_attack_roll(attacker.statblock)
       end
 
-      def calculate_damage(attacker)
-        attack = attacker.attacks.first
-        return if attack.nil?
-
-        damage = attack.calculate_damage(attacker.statblock)
-        damage
+      def calculate_damage(attacker, attack)
+        attack.calculate_damage(attacker.statblock)
       end
 
       def is_hit?(attack_roll, defender)
@@ -87,8 +74,8 @@ module Dnd5e
       end
 
       def winner
-        return @combatant1 if @combatant2.statblock.hit_points <= 0
-        return @combatant2 if @combatant1.statblock.hit_points <= 0
+        return @combatant1 unless @combatant2.statblock.is_alive?
+        return @combatant2 unless @combatant1.statblock.is_alive?
         nil
       end
 
