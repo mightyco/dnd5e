4f412c6e82aa4bb62d1316d5bb0bfd4129d6bf60
mightyco <chuck.mcintyre@gmail.com>
2025-04-06 15:44:11 +0000

    Turn manager works with dice roller

--- lib/dnd5e/core/turn_manager.rb
+++ lib/dnd5e/core/turn_manager.rb
@@ -1,41 +1,40 @@
 require_relative "dice"
+require_relative "dice_roller"
 
 module Dnd5e
   module Core
     class TurnManager
-      class NoCombatantsError < StandardError; end # Define a custom exception
+      class NoCombatantsError < StandardError; end
 
       attr_reader :combatants, :turn_order
+      attr_writer :dice_roller
 
-      def initialize(combatants:)
+      def initialize(combatants:, dice_roller: DiceRoller.new)
         @combatants = combatants
         @turn_order = []
         @current_turn_index = 0
+        @dice_roller = dice_roller
       end
 
       def roll_initiative
-        @turn_order.clear # Clear the turn order before rolling initiative
         @combatants.each do |combatant|
-          initiative_roll = Dice.new(1, 20, modifier: combatant.statblock.ability_modifier(:dexterity)).roll.first
-          combatant.instance_variable_set(:@initiative, initiative_roll)
-          @turn_order << combatant
+          combatant.instance_variable_set(:@initiative, @dice_roller.roll("1d20") + combatant.statblock.ability_modifier(:dexterity))
         end
       end
 
       def sort_by_initiative
-        @turn_order.sort_by! { |combatant| [-combatant.instance_variable_get(:@initiative), -combatant.statblock.dexterity] }
+        @turn_order = @combatants.sort_by { |combatant| [-combatant.instance_variable_get(:@initiative), -combatant.statblock.ability_modifier(:dexterity)] }
       end
 
       def next_turn
-        raise NoCombatantsError, "No combatants in turn order" if @turn_order.empty?
+        raise NoCombatantsError, "No combatants in the turn manager" if @combatants.empty?
 
-        combatant = @turn_order[@current_turn_index]
         @current_turn_index = (@current_turn_index + 1) % @turn_order.size
-        combatant
+        @turn_order[@current_turn_index]
       end
 
       def all_turns_complete?
-        @current_turn_index == 0 && !@turn_order.empty?
+        @current_turn_index == 0 && @turn_order.size > 0
       end
 
       def add_combatant(combatant)
--- test/dnd5e/core/test_turn_manager.rb
+++ test/dnd5e/core/test_turn_manager.rb
@@ -14,64 +14,71 @@ module Dnd5e
         @combatant1 = Character.new(name: "Combatant 1", statblock: @statblock1.deep_copy, attacks: [])
         @combatant2 = Character.new(name: "Combatant 2", statblock: @statblock2.deep_copy, attacks: [])
         @combatants = [@combatant1, @combatant2]
-        @turn_manager = TurnManager.new(combatants: @combatants)
+
+        @logger = Logger.new($stdout)
+        @logger.level = Logger::DEBUG
       end
 
       def test_initialization
-        assert_equal @combatants, @turn_manager.combatants
-        assert_empty @turn_manager.turn_order
+        turn_manager = TurnManager.new(combatants: @combatants)
+        assert_equal @combatants, turn_manager.combatants
+        assert_empty turn_manager.turn_order
       end
 
       def test_roll_initiative
-        @turn_manager.roll_initiative
-        assert_equal 2, @turn_manager.turn_order.size
-        @turn_manager.turn_order.each do |combatant|
-          assert combatant.instance_variable_get(:@initiative).is_a?(Integer)
-          assert combatant.instance_variable_get(:@initiative) >= 1
-          assert combatant.instance_variable_get(:@initiative) <= 20 + combatant.statblock.ability_modifier(:dexterity)
-        end
+        mock_dice_roller = MockDiceRoller.new([10, 15])
+        turn_manager = TurnManager.new(combatants: @combatants, dice_roller: mock_dice_roller)
+        turn_manager.roll_initiative
+        turn_manager.sort_by_initiative
+        assert_equal 2, turn_manager.turn_order.size
+        assert_equal 10 + @combatant1.statblock.ability_modifier(:dexterity), @combatant1.instance_variable_get(:@initiative)
+        assert_equal 15 + @combatant2.statblock.ability_modifier(:dexterity), @combatant2.instance_variable_get(:@initiative)
       end
 
       def test_sort_by_initiative
         mock_dice_roller = MockDiceRoller.new([15, 10])
-        @turn_manager.instance_variable_set(:@dice_roller, mock_dice_roller)
-        @turn_manager.roll_initiative
-        @turn_manager.sort_by_initiative
-        assert_equal [@combatant1, @combatant2], @turn_manager.turn_order
+        turn_manager = TurnManager.new(combatants: @combatants, dice_roller: mock_dice_roller)
+        turn_manager.roll_initiative
+        turn_manager.sort_by_initiative
+        assert_equal [@combatant1, @combatant2], turn_manager.turn_order
       end
 
       def test_sort_by_initiative_with_tie
         mock_dice_roller = MockDiceRoller.new([10, 10])
-        @turn_manager.instance_variable_set(:@dice_roller, mock_dice_roller)
-        @turn_manager.roll_initiative
-        @turn_manager.sort_by_initiative
-        assert_equal [@combatant2, @combatant1], @turn_manager.turn_order
+        turn_manager = TurnManager.new(combatants: @combatants, dice_roller: mock_dice_roller)
+        turn_manager.roll_initiative
+        turn_manager.sort_by_initiative
+        assert_equal [@combatant2, @combatant1], turn_manager.turn_order
       end
 
       def test_next_turn
-        @turn_manager.roll_initiative
-        @turn_manager.sort_by_initiative
-        first_combatant = @turn_manager.turn_order.first
-        assert_equal first_combatant, @turn_manager.next_turn
+        turn_manager = TurnManager.new(combatants: @combatants)
+        turn_manager.roll_initiative
+        turn_manager.sort_by_initiative
+        first_combatant = turn_manager.turn_order.first
+        last_combatant = turn_manager.turn_order.last
+        assert_equal last_combatant, turn_manager.next_turn
       end
 
       def test_next_turn_cycles
-        @turn_manager.roll_initiative
-        @turn_manager.sort_by_initiative
-        first_combatant = @turn_manager.turn_order.first
-        second_combatant = @turn_manager.turn_order.last
-        assert_equal first_combatant, @turn_manager.next_turn
-        assert_equal second_combatant, @turn_manager.next_turn
-        assert_equal first_combatant, @turn_manager.next_turn
+        turn_manager = TurnManager.new(combatants: @combatants)
+        turn_manager.roll_initiative
+        turn_manager.sort_by_initiative
+        first_combatant = turn_manager.turn_order.first
+        second_combatant = turn_manager.turn_order.last
+        assert_equal second_combatant, turn_manager.next_turn
+        assert_equal first_combatant, turn_manager.next_turn
+        assert_equal second_combatant, turn_manager.next_turn
       end
 
       def test_all_turns_complete
-        @turn_manager.roll_initiative
-        @turn_manager.sort_by_initiative
-        @turn_manager.next_turn
-        refute @turn_manager.all_turns_complete?
-        @turn_manager.next_turn
-        assert @turn_manager.all_turns_complete?
+        turn_manager = TurnManager.new(combatants: @combatants)
+        turn_manager.roll_initiative
+        turn_manager.sort_by_initiative
+        turn_manager.next_turn
+        refute turn_manager.all_turns_complete?
+        turn_manager.next_turn
+        assert turn_manager.all_turns_complete?
       end
 
       def test_next_turn_with_no_combatants
@@ -82,15 +89,17 @@ module Dnd5e
       end
 
       def test_add_combatant
+        turn_manager = TurnManager.new(combatants: @combatants)
         new_statblock = Statblock.new(name: "New Statblock", strength: 10, dexterity: 10, constitution: 10, hit_die: "d6", level: 1)
         new_combatant = Character.new(name: "New Combatant", statblock: new_statblock, attacks: [])
-        @turn_manager.add_combatant(new_combatant)
-        assert_includes @turn_manager.combatants, new_combatant
+        turn_manager.add_combatant(new_combatant)
+        assert_includes turn_manager.combatants, new_combatant
       end
 
       def test_remove_combatant
-        @turn_manager.remove_combatant(@combatant1)
-        refute_includes @turn_manager.combatants, @combatant1
+        turn_manager = TurnManager.new(combatants: @combatants)
+        turn_manager.remove_combatant(@combatant1)
+        refute_includes turn_manager.combatants, @combatant1
       end
     end
   end
