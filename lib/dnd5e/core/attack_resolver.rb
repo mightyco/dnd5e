0214228256f269e339bc96aa86037cff3744407b
mightyco <chuck@mightyco.com>
2026-03-08 21:03:08 -0700

    Remove all rubocop:disable directives and refactor for compliance

--- lib/dnd5e/core/attack_resolver.rb
+++ lib/dnd5e/core/attack_resolver.rb
@@ -18,10 +18,12 @@ module Dnd5e
 
       def resolve(attacker, defender, attack, **options)
         if attack.type == :save
-          resolve_save(attacker, defender, attack)
-        else
-          resolve_attack_roll(attacker, defender, attack, options)
+          res = Helpers::SaveResolutionHelper.resolve(attacker, defender, attack)
+          return @result_builder.build(attacker: attacker, defender: defender, attack: attack,
+                                       outcome: res[:outcome], details: res[:details])
         end
+
+        resolve_attack_roll(attacker, defender, attack, options)
       end
 
       private
@@ -49,10 +51,9 @@ module Dnd5e
 
         handle_hit_or_miss(attacker, defender, attack, outcome, dmg_data)
 
-        build_attack_result(attacker: attacker, defender: defender, attack: attack,
-                            roll_data: roll_data, success: outcome[:success],
-                            damage: dmg_data[:damage], is_dead: dmg_data[:is_dead],
-                            damage_rolls: dmg_data[:rolls], damage_modifier: dmg_data[:modifier])
+        @result_builder.build(attacker: attacker, defender: defender, attack: attack,
+                              outcome: { success: outcome[:success], damage: dmg_data[:damage] },
+                              details: build_details(roll_data, dmg_data, defender))
       end
 
       def handle_hit_or_miss(attacker, defender, attack, outcome, dmg_data)
@@ -88,24 +89,12 @@ module Dnd5e
         defender.add_condition(:prone) if save_data[:total] < dc
       end
 
-      def build_attack_result(params)
-        @result_builder.build(attacker: params[:attacker], defender: params[:defender], attack: params[:attack],
-                              outcome: { success: params[:success], damage: params[:damage] },
-                              details: build_details(params))
-      end
-
-      def build_details(params)
-        rd = params[:roll_data]
-        { attack_roll: rd[:total], raw_roll: rd[:raw], modifier: rd[:modifier], is_dead: params[:is_dead],
-          target_ac: params[:defender].statblock.armor_class, rolls: rd[:rolls], advantage: rd[:advantage],
-          disadvantage: rd[:disadvantage], is_crit: rd[:is_crit], damage_rolls: params[:damage_rolls],
-          damage_modifier: params[:damage_modifier] }
-      end
-
-      def resolve_save(attacker, defender, attack)
-        res = Helpers::SaveResolutionHelper.resolve(attacker, defender, attack)
-        @result_builder.build(attacker: attacker, defender: defender, attack: attack,
-                              outcome: res[:outcome], details: res[:details])
+      def build_details(roll_data, dmg_data, defender)
+        { attack_roll: roll_data[:total], raw_roll: roll_data[:raw], modifier: roll_data[:modifier],
+          is_dead: !defender.statblock.alive?, target_ac: defender.statblock.armor_class,
+          rolls: roll_data[:rolls], advantage: roll_data[:advantage],
+          disadvantage: roll_data[:disadvantage], is_crit: roll_data[:is_crit],
+          damage_rolls: dmg_data[:rolls], damage_modifier: dmg_data[:modifier] }
       end
 
       def handle_missing_ac(att, defn, atk)
@@ -113,25 +102,33 @@ module Dnd5e
                               outcome: { success: false, damage: 0 }, details: { target_ac: nil })
       end
 
-      # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
       def apply_attack_damage(attacker, defender, attack, roll_data, outcome)
-        return { damage: 0, is_dead: false, rolls: [], modifier: 0 } unless outcome[:success]
+        return { damage: 0, rolls: [], modifier: 0 } unless outcome[:success]
 
         opt = outcome[:options]
-        is_crit = roll_data[:is_crit]
-        damage_dice = Helpers::DamageRollHelper.calculate_dice(attacker, attack, is_crit, opt)
         mod = Helpers::DamageRollHelper.calculate_modifier(attacker, attack, opt)
-        damage = attack.dice_roller.roll_with_dice(Dice.new(damage_dice.count, damage_dice.sides, modifier: mod))
-        rolls = attack.dice_roller.dice.rolls.dup
+        damage, rolls = calculate_base_damage(attacker, attack, roll_data[:is_crit], opt, mod)
 
-        extra_dmg, extra_rolls = Helpers::DamageRollHelper.roll_extra(attacker, defender, attack, opt)
-        total_dmg = damage + extra_dmg
-        defender.statblock.take_damage(total_dmg)
-        attacker.statblock.record_damage_dealt(total_dmg) if total_dmg.positive?
+        ctx = { attacker: attacker, defender: defender, attack: attack, options: opt }
+        final_dmg, final_rolls = apply_additional_damage(damage, rolls, ctx)
 
-        { damage: total_dmg, is_dead: !defender.statblock.alive?, rolls: rolls + extra_rolls, modifier: mod }
+        { damage: final_dmg, rolls: final_rolls, modifier: mod }
       end
-      # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
+
+      def calculate_base_damage(attacker, attack, is_crit, options, mod)
+        dice = Helpers::DamageRollHelper.calculate_dice(attacker, attack, is_crit, options)
+        damage = attack.dice_roller.roll_with_dice(Dice.new(dice.count, dice.sides, modifier: mod))
+        [damage, attack.dice_roller.dice.rolls.dup]
+      end
+
+      def apply_additional_damage(base_damage, base_rolls, ctx)
+        extra_dmg, extra_rolls = Helpers::DamageRollHelper.roll_extra(ctx[:attacker], ctx[:defender],
+                                                                      ctx[:attack], ctx[:options])
+        total_dmg = base_damage + extra_dmg
+        ctx[:defender].statblock.take_damage(total_dmg)
+        ctx[:attacker].statblock.record_damage_dealt(total_dmg) if total_dmg.positive?
+        [total_dmg, base_rolls + extra_rolls]
+      end
     end
   end
 end
